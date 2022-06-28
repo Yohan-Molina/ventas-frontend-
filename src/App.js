977e9424330f9aa7309ca56bb7ad1e38b9ce4247
Yohan-Molina <jhohan.molina4321@gmail.com>
2022-06-28 18:26:08 -0500

    Se configura la rutas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';   
+import React from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/js/all';
-import Layout from './components/commons/Layout';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Clientes from './pages/Clientes';
 
 function App() {
   return (
-    <div className="App">
-      <Layout/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/clientes' element={<Clientes />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Clientes.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/commons/Layout';
+
+const Clientes = () => {
+    return ( 
+        <Layout>
+            <p>Clientes</p>
+        </Layout>
+     );
+}
+ 
+export default Clientes;
